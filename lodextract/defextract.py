219e3eeac9c69d9e9cac2d129cb9addbc5fc06c8
Laserlicht <13953785+Laserlicht@users.noreply.github.com>
2024-11-17 16:36:18 +0100

    copy

--- lodextract/defextract.py
+++ lodextract/defextract.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 #
 # Copyright (C) 2014  Johannes Schauer <j.schauer@email.de>
+# Copyright (C) 2024  Laserlicht
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
